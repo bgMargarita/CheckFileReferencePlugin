ad545872e258f2837612c333cab20e276e3908ec
bgMargarita <margolyagavrilman@gmail.com>
2019-01-15 22:58:55 +0300

    1 commit

---
+++ src/main/com/intellij/codeInspection/CheckingFileReferencesProvider.java
@@ -0,0 +1,10 @@
+package com.intellij.codeInspection;
+
+
+import com.intellij.codeInspection.InspectionToolProvider;
+import org.jetbrains.annotations.NotNull;
+
+
+public class CheckingFileReferencesProvider implements InspectionToolProvider {
+
+}
