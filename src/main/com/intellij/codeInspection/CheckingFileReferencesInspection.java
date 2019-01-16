20c47d99f8470204a108f279fcbd57f4597b0b8f
bgMargarita <margolyagavrilman@gmail.com>
2019-01-16 13:32:38 +0300

    add  description for problem classes(override)

--- src/main/com/intellij/codeInspection/CheckingFileReferencesInspection.java
+++ src/main/com/intellij/codeInspection/CheckingFileReferencesInspection.java
@@ -1,6 +1,23 @@
 package com.intellij.codeInspection;
 
+import com.intellij.codeInsight.daemon.GroupNames;
+import com.intellij.openapi.diagnostic.Logger;
+import org.jetbrains.annotations.*;
+
+
+
 
 public class CheckingFileReferencesInspection extends BaseJavaLocalInspectionTool {
+    private static final Logger LOG = Logger.getInstance("#CheckingFileReferencesInspection");
+
+
+    private final LocalQuickFix myQuickFix = new MyQuickFix();
+
+    @SuppressWarnings({"WeakerAccess"})
+    @NonNls
+    public String CHECKED_CLASSES = "java.lang.String;java.util.Date";
+    @NonNls
+    private static final String DESCRIPTION_TEMPLATE =
+            "File does not exist in classpath";
 
 }
